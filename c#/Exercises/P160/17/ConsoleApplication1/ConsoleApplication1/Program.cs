7e210a9a12eb249316ad4d5c3bc6cf0630cf71f1
USER1 <USER1@DOMAIN.local>
2017-01-04 13:43:50 +0200

    added 17

---
+++ c#/Exercises/P160/17/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            while (num < 1000 && 99 < num)
+            {
+                num = int.Parse(Console.ReadLine());
+                int h = num / 100;
+                int s = num % 10;
+                int t = (num - s) % 100 / 10;
+            }
+        }
+    }
+}
