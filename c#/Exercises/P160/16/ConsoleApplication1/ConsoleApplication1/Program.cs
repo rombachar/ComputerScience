186fd5fec9221557c45a4200bc6e4fc9384ef8d2
USER1 <USER1@DOMAIN.local>
2017-01-04 12:43:20 +0200

    added files

---
+++ c#/Exercises/P160/16/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool idk = false;
+            int i = 1;
+            while (idk == false)
+            {
+                if (int.Parse(Console.ReadLine()) % 10 != 0)
+                {
+                    idk = true;
+                    Console.WriteLine("The fake is in the " + i + " stack.");
+
+                }
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
